991ae3e0591f1fdd4f1cbdff5a28984639e1d9c3
Ami <ami1996_tm@live.co.uk>
2025-09-23 21:03:40 +0100

    Installed and started building with shadcn/ui

--- src/api/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-  return new Response(
-    JSON.stringify({ name: "Finance Dashboard API", version: "1.0.0" }),
-    { status: 200 }
-  )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
-export default function Home() {
+import { SideMenu } from "@/components"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+
+export default async function Home() {
+  const response = await fetch("http://localhost:3000/api/users/u1")
+  const user = await response.json()
+
   return (
-    <div>
-    </div>
+    <SidebarProvider>
+      <SideMenu />
+      <SidebarInset>
+        <div className="p-4">
+          <h1 className="text-2xl font-bold mb-4">{user.name}</h1>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
